90667e761424bd2ab43cee5400fdf04e05d7522a
Ivan <java@belov.org>
2018-02-09 21:44:09 +0300

    5.3. Создать программу для сортировки массива методом перестановки.

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 09.02.2018
+ */
+
+public class BubbleSort {
+    public static int[] sort(int[] array) {
+        for (int outside = 1; outside < array.length; outside++) {
+            for (int inside = 0; inside < array.length - outside; inside++) {
+                if (array[inside] > array[inside + 1]) {
+                    int temp = array[inside];
+                    array[inside] = array [inside + 1];
+                    array [inside + 1] = temp;
+                }
+            }
+
+        }
+        return array;
+    }
+
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 09.02.2018
+ */
+public class BubbleSortTest {
+    @Test
+    public void whenSortArrayWithTenElementsThenSortedArray() {
+        int[] inputArray = {1, 5, 4, 2, 3, 1, 7, 8, 0, 5};
+        int[] resultArray = BubbleSort.sort(inputArray);
+        int[] expectedArray = {0, 1, 1, 2, 3, 4, 5, 5, 7, 8};
+        assertThat(resultArray, is(expectedArray));
+    }
+
+
+
+}
