c05a0edf12674ab263709ec8ee42f4d97608b61b
Ivan <java@belov.org>
2018-04-27 17:49:28 +0300

    2. Реализовать класс Tracker.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,39 @@
+package ru.job4j.tracker;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 26.04.2018
+ */
+
+public class Item {
+    private String id;
+    private String name;
+    private String desc;
+    private long created;
+    public Item() {
+    }
+    public Item (String name, String desc, long created) {
+        this.name = name;
+        this.desc = desc;
+        this.created = created;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public long getCreated() {
+        return this.created;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,117 @@
+package ru.job4j.tracker;
+
+import java.util.Random;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 26.04.2018
+ */
+
+public class Tracker {
+    /**
+ * Массив для хранение заявок.
+ */
+private final Item[] items = new Item[100];
+private static final Random RN = new Random(100);
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод реализаущий добавление заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    /**
+     * Метод заменяет заявку по заданному id.
+     * @param id идентификатор заявки, которую нужно заменить.
+     * @param item заявка на которую заменяем.
+     */
+    public void replace(String id, Item item) {
+        for (int index = 0; index < this.items.length; index++) {
+            if (items[index].getId().equals(id)) {
+                items[index] = item;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Метод удаляет заявку по заданному id.
+     * @param id идентификатор заявки, которую удаляем.
+     */
+    public void delete(String id) {
+        for (int index = 0; index < this.items.length; index++) {
+            if (items[index].getId().equals(id)) {
+                System.arraycopy(items, index + 1, items, index, items.length - index - 1);
+                items[items.length - 1] = null;
+                position--;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Метод возвращает заявку по заданному id.
+     * @param id идентификатор заявки.
+     * @return возвращаемая заявка.
+     */
+    public Item findById(String id) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод находит заявку по заданному имени.
+     * @param name Имя заявки.
+     * @return Возвращаемая заявка.
+     */
+    public Item findByName(String name) {
+        Item result = null;
+        for (Item item : items) {
+            if (item != null && item.getName().equals(name)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Метод возвращает массив из всех не пустых заявок.
+     * @return Массив не пустых заявок.
+     */
+    public Item[] getAll() {
+        Item[] result = new Item[this.position];
+        for (int index = 0; index != this.position; index++){
+            result[index] = this.items[index];
+        }
+        return result;
+    }
+
+
+
+}
