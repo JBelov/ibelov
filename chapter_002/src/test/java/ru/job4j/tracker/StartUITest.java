b23ea8f62e23ceb0d1aec684972ef2f56b89f536
Ivan <java@belov.org>
2018-04-28 20:16:14 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя.

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,47 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StartUITest {
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();     // создаём Tracker
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем заявку
+        Item item = tracker.add(new Item());
+        //создаём StubInput с последовательностью действий
+        Input input = new StubInput(new String[]{"2", item.getId(), "test name", "desc", "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+    }
+
+    @Test
+    public void whenDeleteThenValueIsNull() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем две заявки
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name 2", "desc 2"));
+        //создаём StubInput с последовательностью действий по удалению первой заявки.
+        Input input = new StubInput(new String[]{"3", item.getId(), "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя второй заявки.
+        assertThat(tracker.getAll()[0].getName(), is("test name 2"));
+    }
+
+
+}
