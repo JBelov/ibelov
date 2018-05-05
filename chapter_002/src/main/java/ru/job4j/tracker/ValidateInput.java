0a534b625f4437a0211b57f7d161e7e79bf3eb8a
Ivan <java@belov.org>
2018-05-05 08:11:24 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,21 +1,38 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 04.05.2018
+ */
+
+public class ValidateInput implements Input {
+
+    private final Input input;
+
+    public ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
+
     public int ask(String question, int[] range) {
         boolean invalid = true;
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please select key from menu.");
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter a valid data");
+                System.out.println("Please enter validate data again.");
             }
-        }
-        while (invalid);
+        } while (invalid);
         return value;
-
     }
 }
+
--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -176,4 +176,19 @@ public class StartUITest {
         );
     }
 
+    @Test
+    public void whenInvalidInput() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[]{"invalid", "1"})
+        );
+        input.ask("Enter", new int[]{1});
+        assertThat(
+                this.out.toString(),
+                is(
+                        String.format("Please enter validate data again.%n")
+                )
+        );
+    }
+
+
 }
