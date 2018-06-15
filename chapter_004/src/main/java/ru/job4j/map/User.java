6c93a07761f9e25283ec65b3768b91608557bff0
Ivan <java@belov.org>
2018-06-15 14:40:53 +0300

    5.1. Создать модель User.

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class User {
+    public String name;
+    public int children;
+    public Calendar birthday;
+}
