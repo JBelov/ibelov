62e2929664288350988eeea082f4feb9fa61b065
Ivan <java@belov.org>
2018-06-15 15:17:48 +0300

    5.3. Переопределить только hashCode.

--- chapter_004/src/main/java/ru/job4j/map/User.java
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -18,4 +18,9 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() + children * 31 + birthday.hashCode() : 0;
+    }
 }
