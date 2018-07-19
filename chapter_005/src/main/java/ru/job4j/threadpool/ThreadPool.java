3830480385f84804aeede93822485be5ce48426a
Ivan <java@belov.org>
2018-07-19 14:06:41 +0300

    002.4.2. Реализовать ThreadPool.

---
+++ chapter_005/src/main/java/ru/job4j/threadpool/ThreadPool.java
@@ -0,0 +1,57 @@
+package ru.job4j.threadpool;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ThreadPool {
+    private final List<Worker> threads = new LinkedList<>();
+    private final Queue<Runnable> tasks = new LinkedBlockingQueue<>();
+
+    ThreadPool() {
+        for (int i = 1; i <= Runtime.getRuntime().availableProcessors(); i++) {
+            Worker worker = new Worker();
+            threads.add(worker);
+            worker.start();
+        }
+    }
+
+    public synchronized void work(Runnable job) {
+        tasks.offer(job);
+    }
+
+    public void shutdown() {
+        for (Worker worker : threads) {
+            worker.shutdown();
+        }
+    }
+
+    private final class Worker extends Thread {
+        private Boolean shutdown = false;
+
+        private void shutdown() {
+            shutdown = true;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Worker Started");
+            while (!isInterrupted() && !shutdown) {
+                if (!tasks.isEmpty()) {
+                    try {
+                        tasks.poll().run();
+                    } catch (NullPointerException n) {
+                    }
+                }
+            }
+            System.out.println("Worker Finished");
+        }
+    }
+}
+
---
+++ chapter_005/src/test/java/ru/job4j/threadpool/ThreadPoolTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.threadpool;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Ivan Belov (ivan@belov.org)
+ * @version $Id$
+ * @since 0.1
+ */
+public class ThreadPoolTest {
+
+    private static AtomicInteger counter = new AtomicInteger(0);
+
+    class NewThread implements Runnable {
+        int id;
+
+        private NewThread(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Started Task #" + id + " " + Thread.currentThread());
+            counter.incrementAndGet();
+            try {
+                Thread.sleep(999);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Ended Task #" + id);
+        }
+    }
+
+    @Test
+    public void workTestMultiThread() {
+        ThreadPool pool = new ThreadPool();
+
+        for (int i = 0; i < 100; i++) {
+            pool.work(new NewThread(i));
+        }
+
+        try {
+            Thread.sleep(5000);
+            pool.shutdown();
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertTrue(counter.get() == 5 * Runtime.getRuntime().availableProcessors());
+    }
+}
